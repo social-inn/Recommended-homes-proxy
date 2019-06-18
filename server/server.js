aff2d37a687bc076e4de8925675321540669cb80
Bill Lou <billloukehao@gmail.com>
2019-06-18 15:40:44 -0700

    Update package.json

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 /* eslint-disable prefer-arrow-callback */
 require('newrelic');
 const express = require('express');
-const request = require('request');
+const axios = require('axios');
 
 const app = express();
 const port = 3333;
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/rooms/:id', function getRoomsServer(req, res) {
-  request.get('http://52.14.173.208:3883/rooms/' + req.params.id)
-    .on('response', (response) => {
-        res.send(response).status(200);
+  axios.get('http://52.14.173.208:3883/rooms/' + req.params.id)
+    .then(function (response) {
+      res.send(response.data).status(200);
     })
-    .on('error', (error) => {
-        res.send('error').status(500);
+    .catch(function (error) {
+      res.send(error).status(500);
     });
 });
 
